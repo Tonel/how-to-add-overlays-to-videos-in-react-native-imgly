17e1cec456e13296a65a37b04dca64745f144fdc
Antonello Zanini <antozanini95@gmail.com>
2022-03-18 18:45:43 +0100

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import VideoWithOverlays from './src/components/VideoWithOverlays/VideoWithOverlays';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const imgLy = require('./src/assets/images/IMG_LY.png');
+
+    return (
+        <View style={styles.container}>
+            <VideoWithOverlays
+                videoComponentProps={{
+                    source: {uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
+                }}
+                text={"IMG.LY"}
+                imageSrc={imgLy}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
